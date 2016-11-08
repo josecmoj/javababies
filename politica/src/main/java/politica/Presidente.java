c4b902bb909cea60efd96f6680558f9db9d21c86
Jonathaluis <jonathaluis@gmail.com>
2016-11-08 09:58:12 -0200

    Adicionado classe Presidente

---
+++ politica/src/main/java/politica/Presidente.java
@@ -0,0 +1,25 @@
+package main.java.politica;
+
+/**
+ * Created by jloliveira1 on 08/11/2016.
+ */
+public class Presidente {
+
+    public Presidente()
+    {
+    }
+
+
+    public void gravarCandidato(String numCandidato){
+        System.out.println(numCandidato.length());
+        if(numCandidato.length() <3)
+        {
+
+            super.NumeroCandidato = Integer.parseInt(numCandidato);
+        }
+        else{
+            super.NumeroCandidato = 0;
+        }
+    }
+
+}
