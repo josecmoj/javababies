ab51f2419e26bfcd81de54b5e71eae052b1efb84
mraykavo <mraykavo@gmail.com>
2016-11-23 08:39:12 -0200

    Alteração da Classe Alimentaçao

--- alimentacao/src/br/com/javababies/alimentacao/Alimentacao.java
+++ alimentacao/src/br/com/javababies/alimentacao/Alimentacao.java
@@ -4,18 +4,22 @@ import javax.swing.JOptionPane;
 
 public class Alimentacao {
 	public static void main(String[] args) {
-		String nome = JOptionPane.showInputDialog("Digite o nome do Cliente: ").toLowerCase();
+		String nome = JOptionPane.showInputDialog("Cadastre Novo Cliente: ").toLowerCase();
 		
 		switch (nome)  {
 		case "marcos":
-			JOptionPane.showMessageDialog(null, "NOVO CLIENTE");
+			//JOptionPane.showMessageDialog(null, "CADASTRO EFETUADO");
+			String identificacao = JOptionPane.showInputDialog("Número do RG: ").toLowerCase();
+			String endereco = JOptionPane.showInputDialog("Endereço ").toLowerCase();
+			String cep = JOptionPane.showInputDialog("CEP ").toLowerCase();
+			JOptionPane.showMessageDialog(null, "CADASTRO EFETUADO");
 			break;
 		default:
-			JOptionPane.showMessageDialog(null, "Erro no Cadastro");		
-				
+			JOptionPane.showMessageDialog(null, "Erro no Cadastro");
+			
 		}
 		
-	
+		
 	}
 
 }
